6388c5b1c8bf004ffb3a213ac6036443b077d2b0
Chad Michel <chadmichel@me.com>
2015-01-28 15:53:34 -0600

    add ability to delete

--- Notes_Xamarin/Pages/NoteEditPage.cs
+++ Notes_Xamarin/Pages/NoteEditPage.cs
@@ -7,13 +7,16 @@ namespace Notes_Xamarin
     public class NoteEditPage : BaseContentPage
     {
         Note _note = null;
-
+        bool _isNew = false;
         Entry entry;
         MyEditor editor;
+        Button deleteCancel;
+        bool _isCanceling = false;
 
         public NoteEditPage(Note note)
         {
             _note = note;
+            _isNew = note.Id <= 0;
             Title = "Note";           
 
             entry = new Entry()
@@ -26,7 +29,25 @@ namespace Notes_Xamarin
             {
                 Keyboard = Keyboard.Create(KeyboardFlags.All),
                 VerticalOptions = LayoutOptions.FillAndExpand,                     
-            };                   
+            };                 
+
+            deleteCancel = new Button()
+            {
+                Text = _isNew ? "Cancel" : "Delete"
+            };
+
+            deleteCancel.Clicked += (object sender, EventArgs e) => 
+            {
+                _isCanceling = true;
+                
+                if (!_isNew)
+                {
+                    _note.IsDeleted = true;
+                    NoteAccessor.Save(_note);
+                }
+
+                Navigation.PopAsync();                    
+            };
                     
             Content = new StackLayout()
             {
@@ -35,7 +56,8 @@ namespace Notes_Xamarin
                     new Label {Text = "Title"},
                     entry,
                     new Label {Text = "Note"},
-                    editor
+                    editor,
+                    deleteCancel
                 },
                 Padding = new Thickness(5, 5, 5, 5)
             };
@@ -45,12 +67,15 @@ namespace Notes_Xamarin
         {
             base.OnDisappearing();           
 
-            _note.Title = entry.Text;
-            _note.Body = editor.Text;
-
-            if (!string.IsNullOrWhiteSpace(_note.Title) || !string.IsNullOrWhiteSpace(_note.Body) || _note.Id > 0)
+            if (!_isCanceling)
             {
-                NoteAccessor.Save(_note);
+                _note.Title = entry.Text;
+                _note.Body = editor.Text;
+
+                if (!string.IsNullOrWhiteSpace(_note.Title) || !string.IsNullOrWhiteSpace(_note.Body) || _note.Id > 0)
+                {
+                    NoteAccessor.Save(_note);
+                }
             }
         }
     }
